fe679e3e2c22d8beb72216fe5365e4df18b72a8b
hamoorapee <kola927@list.ru>
2024-04-15 21:52:54 +0500

    added slf4j and test for output

--- src/test/java/com/demoqa/TextBoxTests.java
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 
-
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -14,17 +14,28 @@ public class TextBoxTests {
     static void configure(){
         baseUrl = "https://demoqa.com";
         Configuration.pageLoadTimeout = 100000;
+        Configuration.browserSize = "1920x1080";
     }
     @Test
     void fillFormTest(){
         open("/text-box");
         $("#userName").setValue("Egor");
+       // $("[id=userName]").setValue("Egor");
         $("#userEmail").setValue("Egor@egor.com");
         $("#currentAddress").setValue("Some address 1");
         $("#permanentAddress").setValue("Some address 2");
         $("#submit").click();
 
         $("#submit").click();
+        $("#output #name").shouldHave(text("Egor"));
+        $("#output #email").shouldHave(text("Egor@egor.com"));
+        $("#output #currentAddress").shouldHave(text("Some address 1"));
+        $("#output #permanentAddress").shouldHave(text("Some address 2"));
+    //  $("#output").$("#name").shouldHave(text("Egor"));
+    //  $("#name").shouldHave(text("Egor"));
+    //  $("#output").shouldHave(text("Egor"));
+    //  $("#output").$("#email").shouldHave(text("Egor"));
+
 
     }
 }

e097974d91a978c9c87d342e698031eb5d08ef67
hamoorapee <kola927@list.ru>
2024-04-15 21:11:39 +0500

    added pageLoadTimeOut and its worked

--- src/test/java/com/demoqa/TextBoxTests.java
+++ src/test/java/com/demoqa/TextBoxTests.java
@@ -4,17 +4,20 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+
+
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTests {
     @BeforeAll
     static void configure(){
-        baseUrl = "https://demoqa.com/text-box";
+        baseUrl = "https://demoqa.com";
+        Configuration.pageLoadTimeout = 100000;
     }
     @Test
     void fillFormTest(){
-        open(baseUrl);
+        open("/text-box");
         $("#userName").setValue("Egor");
         $("#userEmail").setValue("Egor@egor.com");
         $("#currentAddress").setValue("Some address 1");
